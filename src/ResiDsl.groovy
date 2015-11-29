19e0223f84b50b982625d1dd2c08b851e0a5d529
yollalala <yollandasekarrini@gmail.com>
2015-11-29 13:58:42 +0700

    add html template tiki

--- src/ResiDsl.groovy
+++ src/ResiDsl.groovy
@@ -128,10 +128,144 @@ class ResiDsl {
                 script ('', src:'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js')
             }
             body {
-                h1("Memo")
-                h3("Kepada: ${resiDsl.kepadaText}")
-                h3("Dari: ${resiDsl.dariText}")
-                h3("No Resi: ${resiDsl.noResi}")
+                div (class:"container") {
+                    h2 ("Resi Pengiriman")
+                    div (class:"panel panel-default") {
+                        div (class:"panel-body") {
+                            form (role:"form") {
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("No. Resi")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("02 010 547 2731", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Kode wilayah")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("MDC74.00", class:"form-control-static")
+                                     }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Kepada")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("Ani", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Alamat Pengirim")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("Jalan cisitu lama no.123 / 154C", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Telp / Fax")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("085374566905", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Kode wilayah")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("BDO01.00", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Dari")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("Budi", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Alamat Pengirim")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("Jalan Sudirman no.20", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Telp / Fax")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("0853030405", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Isi menurut pengakuan")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("Spare part computer", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Perincian berat volume")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("-", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Services")
+                                    }
+                                    div (class:"col-md-9") {
+                                        div (class:"checkbox") {
+                                          label { input ("SS", type:"checkbox", value:"")}
+                                        }
+                                        div (class:"checkbox checked") {
+                                          label { input ("ONS", type:"checkbox", value:"")}
+                                        }
+//                                        div (class:"checkbox") {
+//                                          label { input ("TDS", type:"checkbox", value:"")}
+//                                        }
+//                                        div (class:"checkbox") {
+//                                          label { input ("REG", type:"checkbox", value:"")}
+//                                        }
+//                                        div (class:"checkbox") {
+//                                          label { input ("Packing", type:"checkbox", value:"")}
+//                                        }
+//                                        div (class:"checkbox") {
+//                                          label { input ("Insurance", type:"checkbox", value:"")}
+//                                        }  
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    div ("", class:"col-md-9")
+                                    div ("Bandung, 1 Desember 2015", class:"col-md-3")
+                                }
+                                div (class:"form-group") {
+                                    div ("", class:"col-md-9")
+                                    div (class:"col-md-3") {
+                                          br()
+                                          b ("Gerai Bandung")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+//                h1("Memo")
+//                h3("Kepada: ${resiDsl.kepadaText}")
+//                h3("Dari: ${resiDsl.dariText}")
+//                h3("No Resi: ${resiDsl.noResi}")
 //                p(resiDsl.body)
 //                 // cycle through the stored section objects and create uppercase/bold section with body
 //                for (s in resiDsl.sections) {
