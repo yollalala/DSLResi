496f8e78a9225dceeeb61784e5d9ec8978728beb
Arieza Nadya <ariezanadya@gmail.com>
2015-11-30 12:33:00 +0700

    spesifikasi jne/tiki

---
+++ src/InputHandler.groovy
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author atia
+ */
+
+class InputHandler {
+    // C:\\Users\\User\\Desktop\\
+    
+    public String bacaFile() {
+        // String string
+            String string = 'C:\\Users\\User\\Desktop\\dslinput.txt'
+            int val = validateInput(string)
+            if (val==1){
+                
+                string = new File('C:\\Users\\User\\Desktop\\dslinput.txt').text
+                string = new GroovyShell().evaluate("ResiDsl.make{" + string + "}")
+            }
+            else {
+                string = "err";
+            }
+            
+            return string; 
+    }
+    
+    public int validateInput(String filename) {
+        int val = 1
+//        def dataList = []
+        def parameters = "biayakirim biayatambahan jumlahtitipan jne tiki kwpenerima kepada alamatpenerima alamatpengirim telppenerima kwpengirim dari telppengirim deskripsi rincianberat service tipebarang".split()
+
+        File file = new File( filename )
+        String stringfile = new File('C:\\Users\\User\\Desktop\\dslinput.txt').text
+
+        if( !file.exists() ) {
+          println "File does not exist"
+        } else {
+          def driveInfo = [:]
+          
+        // loop
+        if (stringfile.contains('tiki')) {
+            file.eachLine { line ->
+                def (key,value) = line.split( ' ' ).collect { it.trim() }
+                if (parameters.contains(key)) {
+                    TikiHandler.addCategory(key)
+                    if (key == "service") {
+                        if (TikiHandler.checkService(value)!=1) {
+                            val = TikiHandler.checkService(value)
+                        }
+                    }
+                } else {
+                    val = 0
+                }
+            }
+            if (TikiHandler.categoryCompleteness()==0) {
+                val = 0
+            }
+        } else { //jne
+            file.eachLine { line ->
+                def (key,value) = line.split( ' ' ).collect { it.trim() }
+                if (parameters.contains(key)) {
+                    JneHandler.addCategory(key)
+                    if (key == "service") {
+                        if (JneHandler.checkService(value)!=1) {
+                            val = JneHandler.checkService(value)
+                        }
+                    } else if (key == "tipebarang") {
+                        if (JneHandler.checkTipeBarang(value)!=1) {
+                            val = JneHandler.checkTipeBarang(value)
+                        }
+                    }
+                } else {
+                    val = 0
+                }
+            }
+            if (JneHandler.categoryCompleteness()==0) {
+                val = 0
+            }
+        }
+            
+          
+//            if( line.trim() ) {
+//              def (key,value) = line.split( ' ' ).collect { it.trim() }
+//              if (parameters.contains(key)) {
+//                  // --------------------------------------------------------------------
+//                  // tiki
+//                  if (stringfile.contains('tiki')) {
+//                    file.eachLine { line ->
+//                      
+//                      TikiHandler.addCategory(key)
+//                      if (key == "service") {
+//                          if (TikiHandler.checkService(value)!=1) {
+//                              val = TikiHandler.checkService(value)
+//                          }
+//                      }
+//                    }
+//                  } else { //jne
+//                    file.eachLine { line ->
+////                      TikiHandler.addCategory(key)
+////                      if (key == "service") {
+////                          if (TikiHandler.checkService(value)!=1) {
+////                              val = TikiHandler.checkService(value)
+////                          }
+////                      }
+//                    }
+//                  }
+//              } else {
+//                  val = 0;
+//              }
+//            }
+//            else {
+//              if( driveInfo ) {
+//                dataList << driveInfo
+//                driveInfo = [:]
+//              }
+//            }
+          return val;
+//          if( driveInfo ) {
+//            dataList << driveInfo
+//          }
+        }
+    }
+}
+
---
+++ src/JneHandler.groovy
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author atia
+ */
+class JneHandler {
+        static def categories = []
+        static List mandatory = ["kepada", "alamatpengirim", "alamatpenerima", "dari", "jumlahtitipan", "rincianberat", "biayakirim", "biayatambahan", "tipebarang", "service"]
+	public static int checkService(String value) {
+            def values = "intracity diplomat SS REG YES OKE international others".split()
+            String con = value.replace("\"","")
+            if (values.contains(con)) {
+                return 1
+            } else {
+                return 5
+            }
+        }
+        
+        public static int categoryCompleteness() {
+            if (categories.containsAll(mandatory)) {
+                return 1
+            } else {
+                return 0
+            }
+        }
+        
+        public static void addCategory(String category) {
+//            println category
+            categories << category
+        }
+        
+        public static void printCategories() {
+            for (int i=0; i<categories.size; i++) {
+                println categories[i]
+            }
+        }
+        
+	public static int checkTipeBarang(String value) {
+            def values = "parcel document".split()
+            String con = value.replace("\"","")
+            if (values.contains(con)) {
+                return 1
+            } else {
+                return 5
+            }
+        }
+}
+
