04b1921e177af430e3fdd2ddc0f8dc62e09668e9
yollalala <yollandasekarrini@gmail.com>
2015-11-30 14:11:17 +0700

    add doJne

--- src/ResiDsl.groovy
+++ src/ResiDsl.groovy
@@ -228,6 +228,22 @@ class ResiDsl {
                                 }
                                 div (class:"form-group") {
                                     label (class:"col-md-3") {
+                                        b ("Jumlah titipan")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("1", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Berat")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("1", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
                                         b ("Isi menurut pengakuan")
                                     }
                                     div (class:"col-md-9") {
@@ -331,7 +347,7 @@ class ResiDsl {
         def xml = new MarkupBuilder(writer)
         xml.html(lang:"en") {
             head {
-                title ("Resi Pengiriman Jne")
+                title ("Resi Pengiriman JNE")
                 meta ('charset':"utf-8")
                 meta ('name':'viewport', 'content':'width=device-width, initial-scale=1')
                 link (rel:"stylesheet", href:"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css")
@@ -339,10 +355,178 @@ class ResiDsl {
                 script ('', src:'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js')
             }
             body {
-                h1("Resi Pengiriman JNE")
-                h3("Kepada: ${resiDsl.kepadaText}")
-                h3("Dari: ${resiDsl.dariText}")
-                h3("No Resi: ${resiDsl.noResi}")
+                div (class:"container") {
+                    h2 ("Resi Pengiriman JNE")
+                    div (class:"panel panel-default") {
+                        div (class:"panel-body") {
+                            form (role:"form") {
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Air Waybill Number")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p (resiDsl.generateNoResi(), class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Shipper")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p (resiDsl.dariText, class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Shipper Address")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p (resiDsl.alamatPengirim, class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Tel. No")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p (resiDsl.telpPenerima, class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Receiver")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p (resiDsl.kepadaText, class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Receiver Address")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p (resiDsl.alamatPenerima, class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Tel. No")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p (resiDsl.telpPengirim, class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Deskripsi of Shipments")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("Spare part computer", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b { u ("Dimensions") }
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Pieces")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("1", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Weight")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("1", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Services")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("Regular service", class:"form-control-static")
+//                                        div (class:"checkbox") {
+//                                          label { input ("SS", type:"checkbox", value:"")}
+//                                        }
+//                                        div (class:"checkbox checked") {
+//                                          label { input ("ONS", type:"checkbox", value:"")}
+//                                        }
+//                                        div (class:"checkbox") {
+//                                          label { input ("TDS", type:"checkbox", value:"")}
+//                                        }
+//                                        div (class:"checkbox") {
+//                                          label { input ("REG", type:"checkbox", value:"")}
+//                                        }
+//                                        div (class:"checkbox") {
+//                                          label { input ("Packing", type:"checkbox", value:"")}
+//                                        }
+//                                        div (class:"checkbox") {
+//                                          label { input ("Insurance", type:"checkbox", value:"")}
+//                                        }  
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Type of Goods")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("Parcel", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Tarif")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("100000", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Other Fee")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p ("10000", class:"form-control-static")
+                                    }
+                                }
+                                div (class:"form-group") {
+                                    label (class:"col-md-3") {
+                                        b ("Amount")
+                                    }
+                                    div (class:"col-md-9") {
+                                        p (("100000".toInteger() + "10000".toInteger()).toString(), class:"form-control-static")
+                                    }
+                                }
+
+                                div (class:"form-group") {
+                                    div ("", class:"col-md-9")
+                                    div ("Bandung, 1 Desember 2015", class:"col-md-3")
+                                }
+                                div (class:"form-group") {
+                                    div ("", class:"col-md-9")
+                                    div (class:"col-md-3") {
+                                          br()
+                                          b ("JNE Signature")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+//                h1("Memo")
+//                h3("Kepada: ${resiDsl.kepadaText}")
+//                h3("Dari: ${resiDsl.dariText}")
+//                h3("No Resi: ${resiDsl.noResi}")
 //                p(resiDsl.body)
 //                 // cycle through the stored section objects and create uppercase/bold section with body
 //                for (s in resiDsl.sections) {
